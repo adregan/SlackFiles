3c2c0a08e95f342a8b825d8fd5c610d8907d29d9
Duncan Regan <duncanregan@gmail.com>
2015-05-24 13:51:22 -0400

    adds our slack utils file

---
+++ src/slack.py
@@ -0,0 +1,29 @@
+import requests
+
+
+class SlackError(Exception): pass
+
+def get_rtm_url(token):
+    rtm_start_url = 'https://slack.com/api/rtm.start?token={token}'.format(
+        token=token
+    )
+
+    resp = requests.post(rtm_start_url)
+
+    if resp.status_code != 200:
+        err = 'Couldn\'t connect to Slack API. Status code: {code}'.format(
+            code=resp.status_code
+        )
+        raise SlackError(err)
+
+    body = resp.json()
+
+    if not body.get('ok', False):
+        err = 'Error authorizing with slack: {error}'.format(
+            error=body.get('error')
+        )
+        raise SlackError(err)
+
+    url = body.get('url')
+
+    return url
