83e56450c4fc71871a357851c016b2baedb17467
Duncan Regan <duncanregan@gmail.com>
2015-05-24 17:27:52 -0400

    handling the connection error here rather than in the run() function

--- src/slack.py
+++ src/slack.py
@@ -3,31 +3,44 @@ import logging
 logger = logging.getLogger('app')
 
 
-class SlackError(Exception): pass
+class SlackError(Exception):
+    pass
 
-def get_rtm_url(token):
+
+def get_rtm_url(token: str) -> str:
+    ''' This function makes a POST request to the slack API's rtm.start method.
+        Pass in the slack token to use for auth (create a bot, rather than use
+        a personal token). If there is any trouble coming from the Slack server
+        or if the token doesn't successfully allow authorization, raises a
+        SlackError. Otherwise, returns the url.
+    '''
+
     rtm_start_url = 'https://slack.com/api/rtm.start?token={token}'.format(
         token=token
     )
 
-    logger.info('Connecting to https://slack.com/api/rtm.start')
-
-    resp = requests.post(rtm_start_url)
+    try:
+        resp = requests.post(rtm_start_url)
+    except requests.exceptions.ConnectionError as err:
+        error = 'ConnectionError connecting to {url}. {error}'.format(
+            url=rtm_start_url,
+            error=err
+        )
+        raise SlackError(error)
+    else:
+        body = resp.json()
+        url = body.get('url')
 
     if resp.status_code != 200:
-        err = 'Couldn\'t connect to Slack API. Status code: {code}'.format(
+        error = 'Couldn\'t connect to Slack API. Status code: {code}'.format(
             code=resp.status_code
         )
-        raise SlackError(err)
-
-    body = resp.json()
+        raise SlackError(error)
 
     if not body.get('ok', False):
-        err = 'Error authorizing with slack: {error}'.format(
+        error = 'Error authorizing with slack: {error}'.format(
             error=body.get('error')
         )
-        raise SlackError(err)
-
-    url = body.get('url')
+        raise SlackError(error)
 
     return url
