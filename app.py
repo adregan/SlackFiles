d182dfc290b7488f8e2eef0dd83cdbdeaaf59cd6
Duncan Regan <duncanregan@gmail.com>
2015-05-24 23:18:43 -0400

    cleaning up the app.py

--- app.py
+++ app.py
@@ -1,60 +1,37 @@
-from configparser import (
-    ConfigParser,
-    NoSectionError,
-    NoOptionError
-)
-import logging
-
-from src.slack import (
-    fetch_from_slack,
-    SlackError,
-    SlackClientProtocol
-)
+from src.slack import fetch_from_slack, SlackError
+from src.clients import SlackClientProtocol
 from src.logs import create_logger
-from src.utils import parse_url
+from src.utils import parse_url, get_config
 import asyncio
+import logging
 
 from autobahn.asyncio.websocket import WebSocketClientFactory
 
-
-def run():
-    create_logger()
-    logger = logging.getLogger('app')
-    file = '/etc/slack.ini'
-
-    config = ConfigParser()
-    success = config.read(file)
-
-    if not success:
-        logger.error(
-            'Configuration file not found: {location}'.format(location=file)
-        )
-        return
-
+@asyncio.coroutine
+def setup():
     try:
-        slack_api_token = config.get('slack', 'slack_api_token')
-    except (NoSectionError, NoOptionError) as error:
-        logger.error(
-            'Error in configuration file: {error}'.format(error=error))
-        return
-    else:
-        logger.info(
-            'Loaded configuration file from: {location}'.format(location=file)
-        )
-
-    try:
-        url = fetch_from_slack('rtm.start', slack_api_token).get('url')
+        body = yield from fetch_from_slack('rtm.start')
     except SlackError as error:
         logger.error(error)
         return
     else:
+        url = body.get('url')
         logger.info('Got Slack webhook URL: {url}.'.format(url=url))
+
+    return url
+
+def run():
+    loop = asyncio.get_event_loop()
+    url = loop.run_until_complete(setup())
+
+    if not url:
+        return
 
     connect = parse_url(url)
 
     factory = WebSocketClientFactory(url, debug=False)
     factory.protocol = SlackClientProtocol
-    loop = asyncio.get_event_loop()
+
     coro = loop.create_connection(
         factory,
         host=connect.host,
@@ -66,4 +43,6 @@ def run():
     loop.close()
 
 if __name__ == '__main__':
+    create_logger()
+    logger = logging.getLogger('app')
     run()
